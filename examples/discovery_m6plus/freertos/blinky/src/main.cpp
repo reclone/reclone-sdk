77ca4996c718e0bdcfb878cd5fdb5be018769e92
angrylemur <jeff@theusch.net>
2016-10-07 18:41:10 -0400

    Finished blinky RTOS C++ example

--- examples/discovery_m6plus/freertos/blinky/src/main.cpp
+++ examples/discovery_m6plus/freertos/blinky/src/main.cpp
@@ -1,23 +1,11 @@
-//
-// This file is part of the GNU ARM Eclipse distribution.
-// Copyright (c) 2014 Liviu Ionescu.
-//
-
-// ----------------------------------------------------------------------------
 
 #include <stdio.h>
 #include "diag/Trace.h"
 #include "stm32f4xx_hal.h"
+#include "CFreeRTOS.h"
+#include "BlinkyTask.h"
 
-// Sample pragmas to cope with warnings. Please note the related line at
-// the end of this function, used to pop the compiler diagnostics status.
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-#pragma GCC diagnostic ignored "-Wreturn-type"
-
-int
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
   // Send a greeting to the trace device (skipped on Release).
   trace_puts("Hello ARM World!");
@@ -26,14 +14,55 @@ main(int argc, char* argv[])
   // at high speed.
   trace_printf("System clock: %uHz\n", SystemCoreClock);
 
-  // Infinite loop
-  while (1)
-  {
 
-  }
-  // Infinite loop, never return.
+  BlinkyTask * blinky = new BlinkyTask(3, 12, 500);
+  blinky->Create("Blinky", configMINIMAL_STACK_SIZE, 2);
+
+  CFreeRTOS::InitHardwareForManagedTasks();
+
+  CFreeRTOS::StartScheduler();
+
+  // Infinite loop
+  while (1) { }
+
+  // If that stops, well then... I guess we're screwed
+  return 0;
 }
 
-#pragma GCC diagnostic pop
+extern "C" void vApplicationMallocFailedHook( void )
+{
+   /* vApplicationMallocFailedHook() will only be called if
+   configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+   function that will get called if a call to pvPortMalloc() fails.
+   pvPortMalloc() is called internally by the kernel whenever a task, queue,
+   timer or semaphore is created.  It is also called by various parts of the
+   demo application.  If heap_1.c or heap_2.c are used, then the size of the
+   heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+   FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+   to query the size of free heap space that remains (although it does not
+   provide information on how the remaining heap might be fragmented). */
+   taskDISABLE_INTERRUPTS();
+   for( ;; );
+}
 
-// ----------------------------------------------------------------------------
+extern "C" void vApplicationStackOverflowHook( xTaskHandle pxTask, signed char *pcTaskName )
+{
+   ( void ) pcTaskName;
+   ( void ) pxTask;
+
+   /* Run time stack overflow checking is performed if
+   configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+   function is called if a stack overflow is detected. */
+   taskDISABLE_INTERRUPTS();
+   for( ;; );
+}
+
+extern "C" void vApplicationTickHook()
+{
+
+}
+
+extern "C" void vApplicationIdleHook()
+{
+
+}
