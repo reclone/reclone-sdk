be00a114d1cda8646091f46605ccb0d13dd7f554
angrylemur <jeff@theusch.net>
2016-11-06 15:16:43 -0500

    PSRAM interface has working writes AND reads!!

--- hdl/reclone_m6_dev/PsramInterface.vhd
+++ hdl/reclone_m6_dev/PsramInterface.vhd
@@ -63,26 +63,39 @@ end PsramInterface;
 
 architecture Behavioral of PsramInterface is
    type psram_state_enum is (PSRAM_ADDR, PSRAM_DATA, PSRAM_WRITE, PSRAM_STALL, PSRAM_ACK, PSRAM_COMPLETE);
-   type ram_type is array (0 to 7) of std_logic_vector (15 downto 0);
+   type ram_type is array (0 to 7) of std_logic_vector (7 downto 0);
 
    signal psram_state      : psram_state_enum := PSRAM_ADDR;
    signal data_out         : std_logic_vector(15 downto 0) := (others => '0');
    signal write_addr       : std_logic_vector(23 downto 0);
+   signal write_addr_next  : std_logic_vector(23 downto 0);
    signal read_addr        : std_logic_vector(23 downto 0);
    signal read_addr_next   : std_logic_vector(23 downto 0);
    signal dbg_out          : std_logic_vector(4 downto 0) := (others => '0');
    signal phase_count      : unsigned(4 downto 0) := "00000";
+   signal writes_allowed   : std_logic := '0';
    
-   signal mem_data         : ram_type :=
+   signal mem_data_h       : ram_type :=
    (
-      0 => "1111000011110000",
-      1 => "0000111100001111",
-      2 => "0011001111001100",
-      3 => "1100110000110011",
-      4 => "1010101010101010",
-      5 => "0101010101010101",
-      6 => "1111111101001000",
-      7 => "0010100011111111"
+      0 => "11110000",
+      1 => "00001111",
+      2 => "00110011",
+      3 => "11001100",
+      4 => "10101010",
+      5 => "01010101",
+      6 => "11111111",
+      7 => "00101000"
+   );
+   signal mem_data_l       : ram_type :=
+   (
+      0 => "11110000",
+      1 => "00001111",
+      2 => "11001100",
+      3 => "00110011",
+      4 => "10101010",
+      5 => "01010101",
+      6 => "01001000",
+      7 => "11111111"
    );
 begin
    FSMC_NWAIT <= '1';
@@ -92,85 +105,81 @@ begin
    
       
    
-   -- FSMC Write process
-   process (FSMC_CLK) is
+   -- FSMC Latch and Write process (rising edge)
+   process (FSMC_CLK, FSMC_NE, FSMC_NBL0, FSMC_NBL1, FSMC_NL, FSMC_NWE, writes_allowed) is
    begin
    
-      -- On rising edge of FSMC clock
-      if rising_edge(FSMC_CLK) then
-      
-         -- If chip select is active
-         if (FSMC_NE = '0') then
-            
-            if (FSMC_NL = '0') then
-               -- Reset phase count and latch the write address
-               phase_count <= "00000";
-               write_addr <= FSMC_A & FSMC_D;
-               --dbg_out <= FSMC_D(4 downto 0);
-            else
-               -- Increment number of FSMC cycles since address latch
-               phase_count <= phase_count + 1;
-               dbg_out <= std_logic_vector(phase_count);
-            end if;
-            
---            if (FSMC_NL = '1' and FSMC_NWE = '0' and phase_count > "00001") then
---               -- Time to write stuff
---               if (FSMC_NBL1 = '0') then
---                  -- Write high byte
---                  mem_data(to_integer(unsigned(write_addr)))(15 downto 8) <= FSMC_D(15 downto 8);
---               end if;
---               if (FSMC_NBL0 = '0') then
---                  -- Write low byte
---                  mem_data(to_integer(unsigned(write_addr)))(7 downto 0) <= FSMC_D(7 downto 0);
---               end if;
---               -- Write whole word for now
---               --mem_data(to_integer(unsigned(write_addr))) <= FSMC_D;
---               
---               -- Auto-increment the address for burst transfers
---               write_addr <= std_logic_vector(unsigned(write_addr) + 1);
---            end if;
-            
-
-         end if;
+      -- On rising edge of FSMC clock, enabled
+      if (rising_edge(FSMC_CLK) and FSMC_NE = '0') then
+         dbg_out(0) <= '1';
+         if (FSMC_NL = '0') then
+            -- Reset phase count and latch the read and write addresses
+            phase_count <= "00000";
+            write_addr_next <= FSMC_A & FSMC_D;
+            read_addr <= FSMC_A & FSMC_D;
+            --dbg_out <= FSMC_D(4 downto 0);
+         else
          
+            if (FSMC_NWE = '0' and writes_allowed = '1') then
+               dbg_out(1) <= '1';
+               
+               -- Time to write stuff
+               if (FSMC_NBL1 = '0') then
+                  -- Write high byte
+                  mem_data_h(to_integer(unsigned(write_addr(2 downto 0)))) <= FSMC_D(15 downto 8);
+                  dbg_out(2) <= '1';
+               end if;
+                  
+               if (FSMC_NBL0 = '0') then
+                  -- Write low byte
+                  mem_data_l(to_integer(unsigned(write_addr(2 downto 0)))) <= FSMC_D(7 downto 0);
+                  dbg_out(3) <= '1';
+               end if;
+
+               -- Auto-increment the address for burst transfers
+               write_addr_next <= std_logic_vector(unsigned(write_addr) + 1);
+            end if;
+         
+            -- Increment number of FSMC cycles since address latch
+            phase_count <= phase_count + 1;
+            -- Update the read address
+            read_addr <= read_addr_next;
+            --dbg_out <= std_logic_vector(phase_count);
+         end if;
+            
       end if;
       
    end process;
    
    
    
-   -- FSMC Read process
-   process (FSMC_CLK) is
+   -- FSMC Read process (falling edge)
+   process (FSMC_CLK, FSMC_NE, FSMC_NOE, phase_count) is
    begin
-      -- On falling edge of FSMC clock
-      if falling_edge(FSMC_CLK) then
+      -- On falling edge of FSMC clock, enabled
+      if (falling_edge(FSMC_CLK) and FSMC_NE = '0') then
          
          -- If in the zeroth phase (determined by rising edge)
          if (phase_count = 0) then
-            -- Copy the read address
+            -- Init the "next" read address (latched on rising edge)
             read_addr_next <= read_addr;
-         elsif (FSMC_NE = '0') then
-            data_out <= mem_data(to_integer(unsigned(read_addr(2 downto 0))));
+         else
+            -- Output the data at the current read_addr
+            data_out(15 downto 8) <= mem_data_h(to_integer(unsigned(read_addr(2 downto 0))));
+            data_out(7 downto 0) <= mem_data_l(to_integer(unsigned(read_addr(2 downto 0))));
          
             if (FSMC_NOE = '0') then
+               -- Auto-increment the read address to support burst transfers
                read_addr_next <= std_logic_vector(unsigned(read_addr) + 1);
-               
             end if;
          end if;
-            
-      end if;
-      
-      if rising_edge(FSMC_CLK) then
-         -- If chip select is active
-         if (FSMC_NE = '0') then
-            
-            if (FSMC_NL = '0') then
-               read_addr <= FSMC_A & FSMC_D;
-            else
-               read_addr <= read_addr_next;
-               --dbg_out(2 downto 0) <= read_addr_next(2 downto 0);
-            end if;
+         
+         if (phase_count > 1) then
+            writes_allowed <= '1';
+         else
+            writes_allowed <= '0';
          end if;
+         write_addr <= write_addr_next;
       end if;
    end process;
 
