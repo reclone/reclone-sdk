2cf12a0c4a375eee48551305f255db93b3c95369
angrylemur <jeff@theusch.net>
2016-11-06 13:42:34 -0500

    Removing the write part fixes the read part

--- hdl/reclone_m6_dev/PsramInterface.vhd
+++ hdl/reclone_m6_dev/PsramInterface.vhd
@@ -66,26 +66,27 @@ architecture Behavioral of PsramInterface is
    type ram_type is array (0 to 7) of std_logic_vector (15 downto 0);
 
    signal psram_state      : psram_state_enum := PSRAM_ADDR;
-   signal data_out         : std_logic_vector(15 downto 0);
+   signal data_out         : std_logic_vector(15 downto 0) := (others => '0');
    signal write_addr       : std_logic_vector(23 downto 0);
    signal read_addr        : std_logic_vector(23 downto 0);
+   signal read_addr_next   : std_logic_vector(23 downto 0);
    signal dbg_out          : std_logic_vector(4 downto 0) := (others => '0');
-   signal phase_count      : integer range 0 to 31 := 0;
+   signal phase_count      : unsigned(4 downto 0) := "00000";
    
    signal mem_data         : ram_type :=
    (
-      "1111000011110000",
-      "0000111100001111",
-      "0011001111001100",
-      "1100110000110011",
-      "1010101010101010",
-      "0101010101010101",
-      "1111111101001000",
-      "0010100011111111"
+      0 => "1111000011110000",
+      1 => "0000111100001111",
+      2 => "0011001111001100",
+      3 => "1100110000110011",
+      4 => "1010101010101010",
+      5 => "0101010101010101",
+      6 => "1111111101001000",
+      7 => "0010100011111111"
    );
 begin
    FSMC_NWAIT <= '1';
-   FSMC_D <= data_out when (FSMC_NOE = '0' and FSMC_NE = '0') else (others => 'Z');
+   FSMC_D <= data_out when (FSMC_NOE = '0') else (others => 'Z');
    --ADR_O <= addr_out;
    dbg <= dbg_out;
    
@@ -103,31 +104,31 @@ begin
             
             if (FSMC_NL = '0') then
                -- Reset phase count and latch the write address
-               phase_count <= 0;
+               phase_count <= "00000";
                write_addr <= FSMC_A & FSMC_D;
-               dbg_out <= FSMC_D(4 downto 0);
+               --dbg_out <= FSMC_D(4 downto 0);
             else
                -- Increment number of FSMC cycles since address latch
                phase_count <= phase_count + 1;
-               --dbg_out <= std_logic_vector(to_unsigned(phase_count, 5));
+               dbg_out <= std_logic_vector(phase_count);
             end if;
             
-            if (FSMC_NL = '1' and FSMC_NWE = '0' and phase_count > 1) then
-               -- Time to write stuff
-               if (FSMC_NBL1 = '0') then
-                  -- Write high byte
-                  mem_data(to_integer(unsigned(write_addr)))(15 downto 8) <= FSMC_D(15 downto 8);
-               end if;
-               if (FSMC_NBL0 = '0') then
-                  -- Write low byte
-                  mem_data(to_integer(unsigned(write_addr)))(7 downto 0) <= FSMC_D(7 downto 0);
-               end if;
-               -- Write whole word for now
-               --mem_data(to_integer(unsigned(write_addr))) <= FSMC_D;
-               
-               -- Auto-increment the address for burst transfers
-               write_addr <= std_logic_vector(unsigned(write_addr) + 1);
-            end if;
+--            if (FSMC_NL = '1' and FSMC_NWE = '0' and phase_count > "00001") then
+--               -- Time to write stuff
+--               if (FSMC_NBL1 = '0') then
+--                  -- Write high byte
+--                  mem_data(to_integer(unsigned(write_addr)))(15 downto 8) <= FSMC_D(15 downto 8);
+--               end if;
+--               if (FSMC_NBL0 = '0') then
+--                  -- Write low byte
+--                  mem_data(to_integer(unsigned(write_addr)))(7 downto 0) <= FSMC_D(7 downto 0);
+--               end if;
+--               -- Write whole word for now
+--               --mem_data(to_integer(unsigned(write_addr))) <= FSMC_D;
+--               
+--               -- Auto-increment the address for burst transfers
+--               write_addr <= std_logic_vector(unsigned(write_addr) + 1);
+--            end if;
             
 
          end if;
@@ -146,16 +147,30 @@ begin
          
          -- If in the zeroth phase (determined by rising edge)
          if (phase_count = 0) then
-            -- Latch the read address
-            read_addr <= FSMC_A & FSMC_D;
+            -- Copy the read address
+            read_addr_next <= read_addr;
          elsif (FSMC_NE = '0') then
             data_out <= mem_data(to_integer(unsigned(read_addr(2 downto 0))));
          
-            if (FSMC_NOE = '0' and phase_count > 0) then
-               read_addr <= std_logic_vector(unsigned(read_addr) + 1);
+            if (FSMC_NOE = '0') then
+               read_addr_next <= std_logic_vector(unsigned(read_addr) + 1);
+               
             end if;
          end if;
             
+      end if;
+      
+      if rising_edge(FSMC_CLK) then
+         -- If chip select is active
+         if (FSMC_NE = '0') then
+            
+            if (FSMC_NL = '0') then
+               read_addr <= FSMC_A & FSMC_D;
+            else
+               read_addr <= read_addr_next;
+               --dbg_out(2 downto 0) <= read_addr_next(2 downto 0);
+            end if;
+         end if;
       end if;
    end process;
 
