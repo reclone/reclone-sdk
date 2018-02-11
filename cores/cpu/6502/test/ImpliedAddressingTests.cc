5da322c1b71621ad424fec3b55516081b0aa61ef
angrylemur <angrylemur@users.noreply.github.com>
2018-02-11 15:26:26 -0500

    Linking verilated.cpp into static lib verilated, and running the verilated model in a unit test

--- cores/cpu/6502/test/ImpliedAddressingTests.cc
+++ cores/cpu/6502/test/ImpliedAddressingTests.cc
@@ -1,13 +1,52 @@
 #include "gtest/gtest.h"
+#include "Vtop_6502.h"
 
 class ImpliedAddressingTests : public ::testing::Test
 {
     public:
         ImpliedAddressingTests() { }
-        virtual ~ImpliedAddressingTests() { }
+        virtual ~ImpliedAddressingTests()
+        {
+            _uut.final();
+        }
+        
+    protected:
+        Vtop_6502 _uut;
 };
 
 TEST_F(ImpliedAddressingTests, AssertPasses)
 {
     ASSERT_TRUE(true);
+}
+
+TEST_F(ImpliedAddressingTests, AssertFails)
+{
+    ASSERT_TRUE(false);
+}
+
+TEST_F(ImpliedAddressingTests, CountUp)
+{
+    _uut.clk = 0;
+    _uut.reset = 1;
+    _uut.enable = 1;
+    _uut.eval();
+    _uut.eval();
+    
+    _uut.clk = 1;
+    _uut.eval();
+    EXPECT_EQ(0, _uut.out);
+    
+    _uut.reset = 0;
+    _uut.clk = 0;
+    _uut.eval();
+    _uut.clk = 1;
+    _uut.eval();
+    EXPECT_EQ(1, _uut.out);
+    
+    _uut.clk = 0;
+    _uut.eval();
+    _uut.clk = 1;
+    _uut.eval();
+    EXPECT_EQ(2, _uut.out);
+
 }
