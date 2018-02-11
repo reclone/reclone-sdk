2a9baa93e0e1308784141a8f207425d47d8c4050
angrylemur <angrylemur@users.noreply.github.com>
2018-02-11 09:07:16 -0500

    Use gnu++17 to build verilated libs

--- cores/cmake/project.cmake
+++ cores/cmake/project.cmake
@@ -3,10 +3,10 @@ project (RecloneCores)
 # Dirty hack to set default VERILATOR_ROOT
 set(VERILATOR_ROOT /usr/share/verilator)
 
-# This is a copy of stuff normally in verilated.mk
+# Verilated code compile flags normally in verilated.mk
 set(CFG_CXXFLAGS_STD_NEWEST -std=gnu++17)
 set(CFG_CXXFLAGS_NO_UNUSED -faligned-new -Wno-bool-operation -Wno-sign-compare -Wno-uninitialized -Wno-unused-but-set-variable -Wno-unused-parameter -Wno-unused-variable -Wno-shadow)
 set(CFG_CXXFLAGS_WEXTRA -Wextra -Wfloat-conversion -Wlogical-op)
 set(VK_CPPFLAGS_ALWAYS -MMD -I${VERILATOR_ROOT}/include -I${VERILATOR_ROOT}/include/vltstd -DVL_PRINTF=printf ${CFG_CXXFLAGS_NO_UNUSED} )
 set(VK_CPPFLAGS_WALL -Wall ${CFG_CXXFLAGS_WEXTRA} -Werror)
-set(VK_CPPFLAGS -I. ${VK_CPPFLAGS_WALL} ${VK_CPPFLAGS_ALWAYS})
+set(VK_CPPFLAGS ${CFG_CXXFLAGS_STD_NEWEST} -I. ${VK_CPPFLAGS_WALL} ${VK_CPPFLAGS_ALWAYS})
