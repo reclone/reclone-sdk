1d7bfb1dd1746dd1634e08ecaf9bf94e166370d5
angrylemur <angrylemur@users.noreply.github.com>
2018-02-18 10:53:38 -0500

    Added perfect6502 as an imported static library that gets downloaded by CMake

---
+++ cores/cpu/6502/test/perfect6502.cmake
@@ -0,0 +1,20 @@
+# Enable ExternalProject CMake module
+include(ExternalProject)
+
+# Download perfect6502
+ExternalProject_Add(
+    perfect6502_external
+    URL https://github.com/reclone/perfect6502/archive/master.zip
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/perfect6502_external
+    # Disable install step
+    INSTALL_COMMAND ""
+)
+
+# Create a perfect6502 target to be used as a dependency by test programs
+add_library(perfect6502 IMPORTED STATIC GLOBAL)
+add_dependencies(perfect6502 perfect6502_external)
+
+# Set perfect6502 properties
+set_target_properties(perfect6502 PROPERTIES
+    "IMPORTED_LOCATION" "${CMAKE_CURRENT_BINARY_DIR}/perfect6502_external/src/perfect6502_external-build/libperfect6502.a"
+)
