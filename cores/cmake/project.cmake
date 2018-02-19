16ade110560f8bbf063d7ae6003c7287183f00b7
angrylemur <angrylemur@users.noreply.github.com>
2018-02-18 21:19:10 -0500

    Added support for both 32- and 64-bit MSYS2

--- cores/cmake/project.cmake
+++ cores/cmake/project.cmake
@@ -14,11 +14,7 @@ endif()
 # Verilated code compile flags normally in verilated.mk
 set(VK_CPPFLAGS -MMD -I${VERILATOR_ROOT}/include -I${VERILATOR_ROOT}/include/vltstd -DVL_PRINTF=printf)
 
-if (MSVC)
-  find_file(VERILATOR verilator PATHS C:\\msys64\\usr\\bin NO_DEFAULT_PATH)
-else()
-  find_file(VERILATOR verilator PATHS /usr/bin NO_DEFAULT_PATH)
-endif(MSVC)
+find_file(VERILATOR verilator PATHS C:\\msys64\\usr\\bin C:\\msys32\\usr\\bin /usr/bin NO_DEFAULT_PATH)
 
 # Overall C++ compile flags
 if (MSVC)
