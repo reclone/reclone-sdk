65b8b18c6223eb1fed1d687a4ebbf7b2f004da93
angrylemur <angrylemur@users.noreply.github.com>
2019-11-27 09:26:46 -0500

    Added CtlEncoder2to10 and Terc4Encoder4to10 modules

---
+++ cores/video/tmds/ctl-encoder-2to10/src/CtlEncoder2to10.v
@@ -0,0 +1,48 @@
+//
+// CtlEncoder2to10 - Encode two DVI-D/HDMI control signal bits into a transition maximized 10-bit word
+//
+// This encoding is used during control periods to convey HSYNC, VSYNC, CTL0, CTL1, CTL2, and CTL3.
+// This encoding is also used for HDMI preambles that indicate whether the next data period is
+// video or a data island.
+//
+// Copyright 2019 Reclone Labs <reclonelabs.com>
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+`default_nettype none
+
+module CtlEncoder2to10
+(
+    input wire[1:0] d,
+    output wire[9:0] q
+);
+
+always @ (d)
+begin
+    case(d)
+        2'b00: q <= 10'b1101010100;
+        2'b01: q <= 10'b0010101011;
+        2'b10: q <= 10'b0101010100;
+        2'b11: q <= 10'b1010101011;
+    endcase
+end
+
+endmodule
---
+++ cores/video/tmds/terc4-encoder-4to10/src/Terc4Encoder4to10.v
@@ -0,0 +1,58 @@
+//
+// Terc4Encoder4to10 - Serialize four HDMI data island bits into TMDS Error Reduction Coding (TERC4)
+//
+// This encoding is used during HDMI data island periods to convey data island packets.
+//
+// Copyright 2019 Reclone Labs <reclonelabs.com>
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+`default_nettype none
+
+module Terc4Encoder4to10
+(
+    input wire[3:0] d,
+    output wire[9:0] q
+);
+
+always @ (d)
+begin
+    case(d)
+        4'b0000: 10'b1010011100;
+        4'b0001: 10'b1001100011;
+        4'b0010: 10'b1011100100;
+        4'b0011: 10'b1011100010;
+        4'b0100: 10'b0101110001;
+        4'b0101: 10'b0100011110;
+        4'b0110: 10'b0110001110;
+        4'b0111: 10'b0100111100;
+        4'b1000: 10'b1011001100;
+        4'b1001: 10'b0100111001;
+        4'b1010: 10'b0110011100;
+        4'b1011: 10'b1011000110;
+        4'b1100: 10'b1010001110;
+        4'b1101: 10'b1001110001;
+        4'b1110: 10'b0101100011;
+        4'b1111: 10'b1011000011;
+    endcase
+end
+
+endmodule
