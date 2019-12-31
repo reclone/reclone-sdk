0103e1e31836e48a0e51e22a588388b2df44d80c
angrylemur <angrylemur@users.noreply.github.com>
2019-12-31 14:12:07 -0500

    Added verilator config for RgbEncoder8to10

--- cores/video/tmds/rgb-encoder-8to10/src/RgbEncoder8to10.v
+++ cores/video/tmds/rgb-encoder-8to10/src/RgbEncoder8to10.v
@@ -54,36 +54,37 @@ wire [3:0] disparity_cnt_next;
 // Use xor to generate a representation of the bit transitions in the input byte
 wire [8:0] xored;
 assign xored[0] = d[0];
-assign xored[1] = xored[0] xor d[1];
-assign xored[2] = xored[1] xor d[2];
-assign xored[3] = xored[2] xor d[3];
-assign xored[4] = xored[3] xor d[4];
-assign xored[5] = xored[4] xor d[5];
-assign xored[6] = xored[5] xor d[6];
-assign xored[7] = xored[6] xor d[7];
+assign xored[1] = d[0] ^ d[1];
+assign xored[2] = d[0] ^ d[1] ^ d[2];
+assign xored[3] = d[0] ^ d[1] ^ d[2] ^ d[3];
+assign xored[4] = d[0] ^ d[1] ^ d[2] ^ d[3] ^ d[4];
+assign xored[5] = d[0] ^ d[1] ^ d[2] ^ d[3] ^ d[4] ^ d[5];
+assign xored[6] = d[0] ^ d[1] ^ d[2] ^ d[3] ^ d[4] ^ d[5] ^ d[6];
+assign xored[7] = d[0] ^ d[1] ^ d[2] ^ d[3] ^ d[4] ^ d[5] ^ d[6] ^ d[7];
 assign xored[8] = 1;
 
 // Use xnor to generate another representation of the bit transitions in the input byte
 wire [8:0] xnored;
 assign xnored[0] = d[0];
-assign xnored[1] = xnored[0] xnor d[1];
-assign xnored[2] = xnored[1] xnor d[2];
-assign xnored[3] = xnored[2] xnor d[3];
-assign xnored[4] = xnored[3] xnor d[4];
-assign xnored[5] = xnored[4] xnor d[5];
-assign xnored[6] = xnored[5] xnor d[6];
-assign xnored[7] = xnored[6] xnor d[7];
+assign xnored[1] = d[0] ~^ d[1];
+assign xnored[2] = d[0] ~^ d[1] ~^ d[2];
+assign xnored[3] = d[0] ~^ d[1] ~^ d[2] ~^ d[3];
+assign xnored[4] = d[0] ~^ d[1] ~^ d[2] ~^ d[3] ~^ d[4];
+assign xnored[5] = d[0] ~^ d[1] ~^ d[2] ~^ d[3] ~^ d[4] ~^ d[5];
+assign xnored[6] = d[0] ~^ d[1] ~^ d[2] ~^ d[3] ~^ d[4] ~^ d[5] ~^ d[6];
+assign xnored[7] = d[0] ~^ d[1] ~^ d[2] ~^ d[3] ~^ d[4] ~^ d[5] ~^ d[6] ~^ d[7];
 assign xnored[8] = 0;
 
 // Use xnor instead of xor if the number of 1 bits is greater than 0 bits, or if the
 // number of 1s equals the number of 0s AND bit zero is a 0.
-wire [3:0] ones = d[0] + d[1] + d[2] + d[3] + d[4] + d[5] + d[6] + d[7];
-wire use_xnored = (ones > 4'd4) or (ones == 4'd4 and d[0] == 1'b0);
+wire [3:0] ones = {3'd0, d[0]} + {3'd0, d[1]} + {3'd0, d[2]} + {3'd0, d[3]} + 
+                  {3'd0, d[4]} + {3'd0, d[5]} + {3'd0, d[6]} + {3'd0, d[7]};
+wire use_xnored = (ones > 4'd4) || (ones == 4'd4 && d[0] == 1'b0);
 wire [8:0] tmds_word = use_xnored ? xnored : xored;
 
 // Number of ones minus the number of zeros in the xor/xnor encoded byte
-wire [3:0] tmds_disparity = -4'd4 + tmds_word[0] + tmds_word[1] + tmds_word[2] + tmds_word[3] + 
-                                    tmds_word[4] + tmds_word[5] + tmds_word[6] + tmds_word[7];
+wire [3:0] tmds_disparity = -4'd4 + {3'd0, tmds_word[0]} + {3'd0, tmds_word[1]} + {3'd0, tmds_word[2]} + {3'd0, tmds_word[3]} + 
+                                    {3'd0, tmds_word[4]} + {3'd0, tmds_word[5]} + {3'd0, tmds_word[6]} + {3'd0, tmds_word[7]};
 
 // This TMDS encoding algorithm was implemented from the Digital Video Interface (DVI) v1.0 specification.
 // Based on 8 input data bits and a (ones minus zeros) disparity counter, produce a 
@@ -92,52 +93,52 @@ wire [3:0] tmds_disparity = -4'd4 + tmds_word[0] + tmds_word[1] + tmds_word[2] +
 // TMDS bit 8: Indicates whether xor(1) or xnor(0) was used to encode bit transitions in TMDS bits 7 to 0
 // TMDS bit 9: Indicates whether TMDS bits 7 to 0 were inverted to improve DC balance of the bitstream
 always @ (*) begin
-    if (disparity_cnt == 4'd0 or tmds_disparity == 4'd0) begin
+    if (disparity_cnt == 4'd0 || tmds_disparity == 4'd0) begin
 
-        q[9] <= !tmds_word[8];
-        q[8] <= tmds_word[8];
+        q[9] = !tmds_word[8];
+        q[8] = tmds_word[8];
 
         if (tmds_word[8] == 1'b1) begin
 
-            q[7:0] <= tmds_word[7:0];
+            q[7:0] = tmds_word[7:0];
 
             // The next disparity count value is the current disparity count,
             // plus the difference: (number of ones minus number of zeros in TMDS bits 0 to 7).
             // From DVI Spec: Cnt(t) = Cnt(t-1) + (N_1{q_m[0:7]} - N_0{q_m[0:7]});
-            disparity_cnt_next <= disparity_cnt + tmds_disparity;
+            disparity_cnt_next = disparity_cnt + tmds_disparity;
 
         end else begin
 
-            q[7:0] <= ~tmds_word[7:0];
+            q[7:0] = ~tmds_word[7:0];
 
             // The next disparity count value is the current disparity count,
             // plus the difference: (number of zeros minus number of ones in TMDS bits 0 to 7).
             // From DVI Spec: Cnt(t) = Cnt(t-1) + (N_0{q_m[0:7]} - N_1{q_m[0:7]});
-            disparity_cnt_next <= disparity_cnt - tmds_disparity;
+            disparity_cnt_next = disparity_cnt - tmds_disparity;
 
         end
     end else begin
         if (tmds_disparity[3] == disparity_cnt[3]) begin
 
-            q[9] <= 1'b1;
-            q[8] <= tmds_word[8];
-            q[7:0] <= ~tmds_word[7:0];
+            q[9] = 1'b1;
+            q[8] = tmds_word[8];
+            q[7:0] = ~tmds_word[7:0];
 
             // The next disparity count value is the current disparity count, plus twice the value of TMDS bit 8,
             // plus the difference: (number of zeros minus number of ones in TMDS bits 0 to 7).
             // From DVI Spec: Cnt(t) = Cnt(t-1) + 2*q_m[8] + (N_0{q_m[0:7]} - N_1{q_m[0:7]});
-            disparity_cnt_next <= disparity_cnt + tmds_word[8] + tmds_word[8] - tmds_disparity;
+            disparity_cnt_next = disparity_cnt + {3'd0, tmds_word[8]} + {3'd0, tmds_word[8]} - tmds_disparity;
 
         end else begin
 
-            q[9] <= 1'b0;
-            q[8] <= tmds_word[8];
-            q[7:0] <= tmds_word[7:0];
+            q[9] = 1'b0;
+            q[8] = tmds_word[8];
+            q[7:0] = tmds_word[7:0];
 
             // The next disparity count value is the current disparity count, minus twice the *inverted* value of TMDS bit 8,
             // plus the difference: (number of ones minus number of zeros in TMDS bits 0 to 7).
             // From DVI Spec: Cnt(t) = Cnt(t-1) - 2*(~q_m[8]) + (N_1{q_m[0:7]} - N_0{q_m[0:7]});
-            disparity_cnt_next <= disparity_cnt - !tmds_word[8] - !tmds_word[8] + tmds_disparity;
+            disparity_cnt_next = disparity_cnt - {3'd0, ~tmds_word[8]} - {3'd0, ~tmds_word[8]} + tmds_disparity;
 
         end
     end
