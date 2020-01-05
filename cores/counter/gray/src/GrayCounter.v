89644df8696b6e20c5c1e2e5ced3df1135fe9ad4
angrylemur <angrylemur@users.noreply.github.com>
2020-01-05 14:30:20 -0500

    Added Gray counter module and its unit tests

---
+++ cores/counter/gray/src/GrayCounter.v
@@ -0,0 +1,56 @@
+//
+// GrayCounter - A counter that outputs Gray code sequences
+//
+// This is a counter, of parameterized width, such that only one bit changes at a time between
+// consecutive count values.  Gray counters are useful for designing data structures (e.g. fifos)
+// that can operate glitch-free in multiple different clock domains.
+//
+//
+// Copyright 2019 Reclone Labs <reclonelabs.com>
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+`default_nettype none
+
+module GrayCounter #(parameter WIDTH = 4)
+(
+    input wire clock,
+    input wire enable,
+    input wire reset,
+    output reg[WIDTH-1:0] grayCount
+);
+
+reg [WIDTH-1:0] binaryCount = {WIDTH{1'b0}};
+wire [WIDTH-1:0] binaryCountNext = binaryCount + 1'd1;
+
+always @ (posedge clock) begin
+    if (reset == 1'b1) begin
+        binaryCount <= {WIDTH{1'b0}};
+        grayCount <= {WIDTH{1'b0}};
+    end else begin
+        if (enable == 1'b1) begin
+            binaryCount <= binaryCountNext;
+            grayCount <= binaryCountNext ^ {1'b0, binaryCountNext[WIDTH-1:1]};
+        end
+    end
+end
+
+endmodule
---
+++ cores/counter/gray/test/GrayCounterTests.cpp
@@ -0,0 +1,236 @@
+//
+// GrayCounterTests - Unit tests for verilated GrayCounter module
+//
+//
+// Copyright 2019 Reclone Labs <reclonelabs.com>
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "gtest/gtest.h"
+#include "VGrayCounter.h"
+
+class GrayCounterTests : public ::testing::Test
+{
+    public:
+        GrayCounterTests() { }
+        virtual ~GrayCounterTests()
+        {
+            _uut.final();
+        }
+        
+    protected:
+        VGrayCounter _uut;
+};
+
+TEST_F(GrayCounterTests, InitialDefaults)
+{
+    ASSERT_EQ(0U, _uut.grayCount);
+    ASSERT_EQ(0U, _uut.GrayCounter__DOT__binaryCount);
+}
+
+TEST_F(GrayCounterTests, Reset)
+{
+    ASSERT_EQ(0U, _uut.grayCount);
+    ASSERT_EQ(0U, _uut.GrayCounter__DOT__binaryCount);
+    
+    _uut.clock = 0;
+    _uut.enable = 1;
+    _uut.reset = 0;
+    _uut.eval();
+    ASSERT_EQ(0x0, _uut.grayCount);
+    
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x1, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x3, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.reset = 1;
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x0, _uut.grayCount);
+    ASSERT_EQ(0U, _uut.GrayCounter__DOT__binaryCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.reset = 0;
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x1, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+}
+
+TEST_F(GrayCounterTests, Enable)
+{
+    ASSERT_EQ(0U, _uut.grayCount);
+    ASSERT_EQ(0U, _uut.GrayCounter__DOT__binaryCount);
+    
+    _uut.clock = 0;
+    _uut.enable = 1;
+    _uut.reset = 0;
+    _uut.eval();
+    ASSERT_EQ(0x0, _uut.grayCount);
+    
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x1, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x3, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.enable = 0;
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x3, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.enable = 1;
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x2, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+}
+
+TEST_F(GrayCounterTests, FourBitGrayCount)
+{
+    _uut.clock = 0;
+    _uut.enable = 1;
+    _uut.reset = 0;
+    _uut.eval();
+    ASSERT_EQ(0x0, _uut.grayCount);
+    
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x1, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x3, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x2, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x6, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x7, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x5, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x4, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xC, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xD, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xF, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xE, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xA, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0xB, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x9, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x8, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x0, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+    _uut.clock = 1;
+    _uut.eval();
+    ASSERT_EQ(0x1, _uut.grayCount);
+    _uut.clock = 0;
+    _uut.eval();
+
+}
