607f6fc5b6825572e9130440330a983d5bdb4aef
angrylemur <angrylemur@users.noreply.github.com>
2020-02-22 18:39:43 -0500

    Fixed definition of PAL color bars to meet PAL geometry and YUV colors

--- cores/video/pal/test-pattern/color-bars/src/PalColorBars.v
+++ cores/video/pal/test-pattern/color-bars/src/PalColorBars.v
@@ -52,36 +52,36 @@ always @ (posedge palClock) begin
     burstDelayed <= burst;
     burstPhaseDelayed <= burstPhase;
 
-    if (vPos < 10'd324) begin
+    if (vPos < 10'd383) begin
         // Main set of seven color bars
-        // 720/7=103 pixels per bar
-        // 17 overscan pixels on left and right sides
-        if (hPos < 10'd120) begin
+        // 768/7=110 pixels per bar
+        // 77.5 overscan pixels on left and right sides
+        if (hPos < 10'd187) begin
             // 75% white
             y <= 9'd235;
             u <= 9'h00;
             v <= 9'h00;
-        end else if (hPos < 10'd223) begin
+        end else if (hPos < 10'd297) begin
             // 75% yellow
             y <= 9'd169;
             u <= -9'd83;
             v <= 9'd19;
-        end else if (hPos < 10'd326) begin
+        end else if (hPos < 10'd407) begin
             // 75% cyan
             y <= 9'd134;
             u <= 9'd28;
             v <= -9'd117;
-        end else if (hPos < 10'd429) begin
+        end else if (hPos < 10'd517) begin
             // 75% green
             y <= 9'd112;
             u <= -9'd55;
             v <= -9'd98;
-        end else if (hPos < 10'd532) begin
+        end else if (hPos < 10'd627) begin
             // 75% magenta
             y <= 9'd79;
             u <= 9'd55;
             v <= 9'd98;
-        end else if (hPos < 10'd635) begin
+        end else if (hPos < 10'd737) begin
             // 75% red
             y <= 9'd57;
             u <= -9'd28;
@@ -92,34 +92,34 @@ always @ (posedge palClock) begin
             u <= 9'd83;
             v <= -9'd19;
         end
-    end else if (vPos < 10'd363) begin
+    end else if (vPos < 10'd440) begin
         // Strip of blue, magenta, cyan, and white castellations
-        if (hPos < 10'd120) begin
+        if (hPos < 10'd187) begin
             // 75% blue
             y <= 9'd22;
-            u <= -9'd61;
-            v <= 9'd59;
-        end else if (hPos < 10'd223) begin
+            u <= 9'd83;
+            v <= -9'd19;
+        end else if (hPos < 10'd297) begin
             // black
             y <= 9'd0;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd326) begin
+        end else if (hPos < 10'd407) begin
             // 75% magenta
             y <= 9'd79;
-            u <= 9'd53;
-            v <= 9'd100;
-        end else if (hPos < 10'd429) begin
+            u <= 9'd55;
+            v <= 9'd98;
+        end else if (hPos < 10'd517) begin
             // black
             y <= 9'd0;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd532) begin
+        end else if (hPos < 10'd627) begin
             // 75% cyan
             y <= 9'd134;
-            u <= -9'd114;
-            v <= -9'd40;
-        end else if (hPos < 10'd635) begin
+            u <= 9'd28;
+            v <= -9'd117;
+        end else if (hPos < 10'd737) begin
             // black
             y <= 9'd0;
             u <= 9'd0;
@@ -132,39 +132,39 @@ always @ (posedge palClock) begin
         end
     end else begin
         // Calibration ranges and pluge pulse
-        // Starts with four wider bars 5*103/4=129 pixels per wide bar
-        // 17 overscan pixels on left and right sides
-        if (hPos < 10'd146) begin
+        // Starts with four wider bars 5*110/4=137 pixels per wide bar
+        // 77.5 overscan pixels on left and right sides
+        if (hPos < 10'd216) begin
             // -U at the same magnitude as color burst
             y <= 9'd0;
             u <= -9'd64;
             v <= 9'd0;
-        end else if (hPos < 10'd275) begin
+        end else if (hPos < 10'd353) begin
             // 100% white
             y <= 9'd255;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd404) begin
+        end else if (hPos < 10'd490) begin
             // +V at the same magnitude as color burst
             y <= 9'd0;
             u <= 9'd0;
             v <= 9'd64;
-        end else if (hPos < 10'd532) begin
+        end else if (hPos < 10'd627) begin
             // black
             y <= 9'd0;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd566) begin
+        end else if (hPos < 10'd664) begin
             // 4% below black
             y <= -9'd10;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd601) begin
+        end else if (hPos < 10'd700) begin
             // black
             y <= 9'd0;
             u <= 9'd0;
             v <= 9'd0;
-        end else if (hPos < 10'd635) begin
+        end else if (hPos < 10'd737) begin
             // 4% above black
             y <= 9'd10;
             u <= 9'd0;
