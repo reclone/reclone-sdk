013318ea0fde5e8ae6c981419d55ba4261151093
angrylemur <angrylemur@users.noreply.github.com>
2021-05-27 14:21:05 -0400

    Initial draft of Cpu6502 which encapsulates a Cpu6502MicrocodeRom and a Cpu65xxMicroArchitecture

---
+++ cores/cpu/65xx/6502/src/Cpu6502.v
@@ -0,0 +1,99 @@
+//
+// Cpu6502 - A hopefully cycle-accurate implementation of the MOS 6502
+//
+//
+// Copyright 2021 Reclone Labs <reclonelabs.com>
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+`default_nettype none
+
+module Cpu6502
+(
+    input clock,
+    input enable,
+    input reset,
+
+    input wire [7:0] dataIn,
+    output wire [7:0] dataOut,
+    output wire dataWriteEnable,
+    output wire [15:0] address,
+
+    input wire nNMI,
+    input wire nIRQ,
+    input wire nRESET
+);
+
+wire [9:0]  uCodeAddress;
+wire [1:0]  uCodeAddrBusMux;
+wire        uCodeDataWriteEnable;
+wire [1:0]  uCodeDataBusMux;
+wire [3:0]  uCodeAluOperation;
+wire [3:0]  uCodeAluOperandAMux;
+wire [2:0]  uCodeAluOperandBMuxOrOpExtension;
+wire [3:0]  uCodeAluResultStorage;
+wire        uCodeIncrementAddr;
+wire        uSeqBranchCondition;
+wire [9:0]  uSeqBranchAddr;
+
+Cpu65xxMicroArchitecture uArch
+(
+    .clock(clock),
+    .enable(enable),
+    .reset(reset),
+    .dataIn(dataIn),
+    .dataOut(dataOut),
+    .dataWriteEnable(dataWriteEnable),
+    .address(address),
+    .nNMI(nNMI),
+    .nIRQ(nIRQ),
+    .nRESET(nRESET),
+    .uCodeAddress(uCodeAddress),
+    .uCodeAddrBusMux(uCodeAddrBusMux),
+    .uCodeDataWriteEnable(uCodeDataWriteEnable),
+    .uCodeDataBusMux(uCodeDataBusMux),
+    .uCodeAluOperation(uCodeAluOperation),
+    .uCodeAluOperandAMux(uCodeAluOperandAMux),
+    .uCodeAluOperandBMuxOrOpExtension(uCodeAluOperandBMuxOrOpExtension),
+    .uCodeAluResultStorage(uCodeAluResultStorage),
+    .uCodeIncrementAddr(uCodeIncrementAddr),
+    .uSeqBranchCondition(uSeqBranchCondition),
+    .uSeqBranchAddr(uSeqBranchAddr)
+);
+
+Cpu6502MicrocodeRom uCode
+(
+    .clock(clock),
+    .enable(enable),
+    .address(uCodeAddress),
+    .addrBusMux(uCodeAddrBusMux),
+    .dataBusWriteEnable(uCodeDataWriteEnable),
+    .dataBusMux(uCodeDataBusMux),
+    .aluOperation(uCodeAluOperation),
+    .aluOperandA(uCodeAluOperandAMux),
+    .aluOperandB(uCodeAluOperandBMuxOrOpExtension),
+    .aluResultStorage(uCodeAluResultStorage),
+    .incrementAddr(uCodeIncrementAddr),
+    .uSeqBranchCondition(uSeqBranchCondition),
+    .uSeqBranchAddr(uSeqBranchAddr)
+);
+
+endmodule
--- cores/cpu/65xx/6502/src/Cpu6502MicrocodeRom.v
+++ cores/cpu/65xx/6502/src/Cpu6502MicrocodeRom.v
@@ -27,7 +27,7 @@
 `default_nettype none
 
 `include "Cpu6502Opcodes.vh"
-`include "Cpu6502MicrocodeConstants.vh"
+`include "Cpu65xxMicrocodeConstants.vh"
 
 module Cpu6502MicrocodeRom
 (
@@ -94,7 +94,7 @@ begin
                 d <= {ADDR_IND, DATA_READ, DATA_PCH, ALU_OP_AND, ALU_A_FD, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_RESET + 10'd1};
             USEQ_ADDR_RESET + 10'd1:
                 // PCL <= ($FFFC)
-                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_ADD, ALU_A_FD, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_RESET + 10'd2};
+                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_IADD, ALU_A_FD, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_RESET + 10'd2};
             USEQ_ADDR_RESET + 10'd2:
                 // OP <= (PC), I <= 1
                 d <= {ADDR_PC, DATA_READ, DATA_NULL, ALU_OP_SETBIT, ALU_A_P, I_BIT_IN_P, ALU_O_P, ADDR_INC, USEQ_BR_IF_CLR, USEQ_OP};
@@ -114,7 +114,7 @@ begin
                 d <= {ADDR_SP, DATA_WRITE, DATA_IMM, ALU_OP_DEC, ALU_A_S, ALU_B_ZERO, ALU_O_S, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_IRQ + 10'd4};
             USEQ_ADDR_IRQ + 10'd4:
                 // PCL <= ($FFFE)
-                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_ADD, ALU_A_FF, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_IRQ + 10'd5};
+                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_IADD, ALU_A_FF, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_IRQ + 10'd5};
             USEQ_ADDR_IRQ + 10'd5:
                 // PCH <= ($FFFF)
                 d <= {ADDR_IND, DATA_READ, DATA_PCH, ALU_OP_AND, ALU_A_FF, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_IRQ + 10'd6};
@@ -140,7 +140,7 @@ begin
                 d <= {ADDR_IND, DATA_READ, DATA_PCH, ALU_OP_OR, ALU_A_FB, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_NMI + 10'd5};
             USEQ_ADDR_NMI + 10'd5:
                 // PCL <= ($FFFA)
-                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_ADD, ALU_A_FB, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_FETCH};
+                d <= {ADDR_IND, DATA_READ, DATA_PCL, ALU_OP_IADD, ALU_A_FB, ALU_B_FF, ALU_O_ADDR, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_FETCH};
 
             /* WASTE1 - Waste one cycle doing nothing, then fetch */
             USEQ_ADDR_WASTE1:
@@ -217,7 +217,7 @@ begin
 
             USEQ_ADDR_BR_REL:
                 // OP <= PC (dummy), PCL <= PCL + IMM, if no carry skip PCH fixup and goto fetch
-                d <= {ADDR_PC, DATA_READ, DATA_NULL, ALU_OP_ADD, ALU_A_PCL, ALU_B_IMM, ALU_O_PCL, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_FETCH};
+                d <= {ADDR_PC, DATA_READ, DATA_NULL, ALU_OP_RADD, ALU_A_PCL, ALU_B_IMM, ALU_O_PCL, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_FETCH};
             USEQ_ADDR_BR_REL + 10'd1:
                 // Fix PCH:
                 //  If IMM[7] == 1: PCH <= PCH - 1
@@ -363,10 +363,10 @@ begin
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_AND, ALU_A_ZERO, ALU_B_ZERO, ALU_O_NULL, ADDR_INC, USEQ_BR_IF_CLR, {UPAGE_OP, LDA_ZPG_X, 1'b1}};
             {UPAGE_OP, LDA_ZPG_X, 1'b1}:
                 // IMM <= X + IMM
-                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_X, ALU_B_IMM, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ZPG_X};
+                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_X, ALU_B_IMM, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ZPG_X};
             USEQ_ADDR_LDA_ZPG_X:
                 // IMM <= ({$00, IMM})
-                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_X, ALU_B_IMM, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ZPG_X + 10'd1};
+                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_X, ALU_B_IMM, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ZPG_X + 10'd1};
             USEQ_ADDR_LDA_ZPG_X + 10'd1:
                 // A <= IMM, OP <= (PC), PC <= PC + 1
                 d <= {ADDR_PC, DATA_READ, DATA_NULL, ALU_OP_OR, ALU_A_IMM, ALU_B_ZERO_FLG, ALU_O_A, ADDR_INC, USEQ_BR_IF_CLR, USEQ_OP};
@@ -386,7 +386,7 @@ begin
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_AND, ALU_A_ZERO, ALU_B_ZERO, ALU_O_NULL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ABS_X};
             USEQ_ADDR_LDA_ABS_X:
                 // IMM <= (PC), PC <= PC + 1, INDL <= X + IMM, if no carry skip next instruction
-                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_X, ALU_B_IMM, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
+                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_X, ALU_B_IMM, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
             USEQ_ADDR_LDA_ABS_X + 10'd1:
                 // IMM <= IMM + 1
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_INC, ALU_A_IMM, ALU_B_ZERO, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
@@ -396,7 +396,7 @@ begin
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_AND, ALU_A_ZERO, ALU_B_ZERO, ALU_O_NULL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_ABS_Y};
             USEQ_ADDR_LDA_ABS_Y:
                 // IMM <= (PC), PC <= PC + 1, INDL <= Y + IMM, if no carry skip next instruction
-                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_Y, ALU_B_IMM, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
+                d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_Y, ALU_B_IMM, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
             USEQ_ADDR_LDA_ABS_Y + 10'd1:
                 // IMM <= IMM + 1
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_INC, ALU_A_IMM, ALU_B_ZERO, ALU_O_IMM, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_IMM_INDL};
@@ -413,7 +413,7 @@ begin
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_AND, ALU_A_ZERO, ALU_B_ZERO, ALU_O_NULL, ADDR_INC, USEQ_BR_IF_CLR, {UPAGE_OP, LDA_X_IND, 1'b1}};
             {UPAGE_OP, LDA_X_IND, 1'b1}:
                 // ZPG <= X + IMM
-                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_X, ALU_B_IMM, ALU_O_ZPG, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_X_IND};
+                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_X, ALU_B_IMM, ALU_O_ZPG, ADDR_NOP, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_X_IND};
             USEQ_ADDR_LDA_X_IND:
                 // INDL <= ({$00,ZPG}), ZPG <= ZPG + 1
                 d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_COPY, ALU_A_DI, ALU_B_ZERO, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_CLR, USEQ_ADDR_LDA_X_IND + 10'd1};
@@ -429,7 +429,7 @@ begin
                 d <= {ADDR_PC, DATA_READ, DATA_IMM, ALU_OP_COPY, ALU_A_DI, ALU_B_ZERO, ALU_O_ZPG, ADDR_INC, USEQ_BR_IF_CLR, {UPAGE_OP, LDA_IND_Y, 1'b1}};
             USEQ_ADDR_LDA_IND_Y:
                 // INDL <= ({$00,ZPG}) + Y, ZPG <= ZPG + 1, detect page crossing
-                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_ADD, ALU_A_Y, ALU_B_DI, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_SET, USEQ_ADDR_LDA_IND_Y + 10'd2};
+                d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_IADD, ALU_A_Y, ALU_B_DI, ALU_O_INDL, ADDR_INC, USEQ_BR_IF_SET, USEQ_ADDR_LDA_IND_Y + 10'd2};
             USEQ_ADDR_LDA_IND_Y + 10'd1:
                 // No page crossing:    INDH <= ({$00,ZPG})
                 d <= {ADDR_ZPG, DATA_READ, DATA_IMM, ALU_OP_COPY, ALU_A_DI, ALU_B_ZERO, ALU_O_INDH, ADDR_NOP, USEQ_BR_IF_CLR, {UPAGE_OP, LDA_IND_Y, 1'b1}};
